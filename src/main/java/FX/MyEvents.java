ed51f1487544b5e5fb2b4e23348d0f47fd4363b4
kielek <kielek>
2019-10-25 18:06:09 +0200

    s

--- src/main/java/FX/MyEvents.java
+++ src/main/java/FX/MyEvents.java
@@ -1,15 +1,16 @@
 package FX;
 
+import javafx.animation.PathTransition;
 import javafx.animation.RotateTransition;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -39,6 +40,7 @@ public class MyEvents {
 
         return new Group(circle, text);
     }
+
     public static Group boxEvent(){
         Box box = new Box();
         box.setWidth(150.0);
@@ -77,7 +79,70 @@ public class MyEvents {
 
         return new Group(box, text);
     }
+
+    public static Group circleEvent2(){
+        Circle circle = new Circle();
+        circle.setCenterX(300.0f);
+        circle.setCenterY(135.0f);
+        circle.setRadius(25.0f);
+        circle.setFill(Color.BROWN);
+        circle.setStrokeWidth(20);
+
+
+        MoveTo moveTo = new MoveTo(208, 71);
+        LineTo line1 = new LineTo(421, 161);
+        LineTo line2 = new LineTo(226,232);
+        LineTo line3 = new LineTo(332,52);
+        LineTo line4 = new LineTo(369, 250);
+        LineTo line5 = new LineTo(208, 71);
+
+        Path path = new Path();
+        path.getElements().add(moveTo);
+        path.getElements().addAll(line1, line2, line3, line4, line5);
+
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(1000));
+        pathTransition.setNode(circle);
+        pathTransition.setPath(path);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(50);
+        pathTransition.setAutoReverse(false);
+
+        Rectangle playButton = new Rectangle();
+        playButton.setX(260);
+        playButton.setY(250);
+        playButton.setWidth(20);
+        playButton.setHeight(20);
+
+        circle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Hello World");
+                circle.setFill(Color.DARKSLATEBLUE);
+            }
+        });
+        playButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                System.out.println("Start");
+                pathTransition.play();
+            }
+        }));
+
+        Rectangle stopButton = new Rectangle();
+        stopButton.setX(300);
+        stopButton.setY(250);
+        stopButton.setWidth(20.0f);
+        stopButton.setHeight(20.0f);
+        stopButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                System.out.println("Stop");
+                pathTransition.stop();
+            }
+        }));
+
+        return new Group(circle, playButton, stopButton);
+    }
     public static Group getGroup(){
-        return new Group(boxEvent());
+        return new Group(circleEvent2());
     }
 }
--- src/main/java/FX/Start.java
+++ src/main/java/FX/Start.java
@@ -3,7 +3,8 @@ package FX;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -13,14 +14,14 @@ public class Start extends Application {
     public void start(Stage primaryStage) {
 
         //Tworzymy obiekt grupy
-        //TextField textField1 = new TextField();
-        Group group = new Group(MyEvents.getGroup());
+        //Control btn = new Button();
+        Group group = new Group(MyEvents.circleEvent2());
 
         //Tworzymy scene poprzez podanie klasy Group, wysokosci i szerokosci
         Scene scene = new Scene(group ,600, 400);
 
         //Ustawiamy kolor tła
-        scene.setFill(Color.RED);
+        scene.setFill(Color.WHITE);
 
         //Ustawiamy tytuł okienka
         primaryStage.setTitle("Kurs java fx");
